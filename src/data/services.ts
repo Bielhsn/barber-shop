9f0e5a5d7369fc66304fa4c2c80a3fd066bb102e
Gabriel Henrique Souza do Nascimento <113404490+Bielhsn@users.noreply.github.com>
2025-03-14 21:24:07 -0300

    trocando img barba

--- src/data/services.ts
+++ src/data/services.ts
@@ -8,7 +8,7 @@ export const services: Service[] = [
     duration: 30,
     priceWeek: 35, // Preço de terça a quinta
     priceWeekend: 40, // Preço de sexta a domingo
-    image: 'https://images.unsplash.com/photo-1585747860715-2ba37e788b70?auto=format&fit=crop&q=80&w=800'
+    image: 'https://images.unsplash.com/photo-1493256338651-d82f7acb2b38?auto=format&fit=crop&q=80&w=800'
   },
   {
     id: '2',
@@ -17,7 +17,7 @@ export const services: Service[] = [
     duration: 20,
     priceWeek: 40,
     priceWeekend: 40,
-    image: 'https://images.unsplash.com/photo-1621605815971-fbc98d665033?auto=format&fit=crop&q=80&w=800'
+    image: 'https://images.unsplash.com/photo-1596728325488-58c87691e9af?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
   {
     id: '3',
@@ -26,6 +26,6 @@ export const services: Service[] = [
     duration: 45,
     priceWeek: 60,
     priceWeekend: 60,
-    image: 'https://images.unsplash.com/photo-1493256338651-d82f7acb2b38?auto=format&fit=crop&q=80&w=800'
+    image: 'https://images.unsplash.com/photo-1585747860715-2ba37e788b70?auto=format&fit=crop&q=80&w=800'
   }
 ];
